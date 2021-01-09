e8b573852149c3cc84075707af868b06ed1ca2b8
rohit15796 <40022256+rohit15796@users.noreply.github.com>
2021-01-09 13:03:55 +0530

    added sql file
    database file

---
+++ users_data.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2021 at 08:30 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `emp_details`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_data`
+--
+
+CREATE TABLE `users_data` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `mobileno` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `experience` int(11) NOT NULL,
+  `skills` varchar(255) NOT NULL,
+  `resume` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Employee Details';
+
+--
+-- Dumping data for table `users_data`
+--
+
+INSERT INTO `users_data` (`id`, `uname`, `mobileno`, `email`, `age`, `experience`, `skills`, `resume`) VALUES
+(1, 'employee1', 123466789, 'test@gmail.com', 45, 5, 'PHP,CSS', 'test.jpg'),
+(2, 'employee2', 123466789, 'test2@gmail.com', 45, 5, 'PHP,CSS', 'file.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users_data`
+--
+ALTER TABLE `users_data`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
